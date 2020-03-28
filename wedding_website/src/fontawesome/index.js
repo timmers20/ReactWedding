4ecfd58ec0f8dda2ecdf8a73bd89016e2c3a411d
Daniel Timmers <43832218+timmers20@users.noreply.github.com>
2020-03-28 18:32:57 +0000

    Initial Commit

---
+++ wedding_website/src/fontawesome/index.js
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronRight,
+  faChevronDown,
+  faCheck,
+  faTimes,
+  faSpinner
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChevronRight, faChevronDown, faCheck, faTimes, faSpinner);
